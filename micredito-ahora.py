81bd874aa5f31d87d15d7d475a2bbb3b3922e6ad
maaraya <72613106+maaraya@users.noreply.github.com>
2021-08-01 22:44:28 -0400

    Update micredito-ahora.py

--- micredito-ahora.py
+++ micredito-ahora.py
@@ -22,7 +22,7 @@ def evaluar(valor):
 
 def main():
     st.title("Quiero mi crédito ahora!")
-    menu = ['Evaluación', 'Acerca de nuestro proyecto', 'Test formulario']
+    menu = ['Evaluación', 'Test formulario','Acerca de nuestro proyecto' ]
     choice = st.sidebar.selectbox('Menu',menu)
 
     if choice == 'Evaluación':
@@ -31,158 +31,137 @@ def main():
         st.text('Realice su pre-evaluación de financiamiento complete los campos solicitados')
         with st.form(key='evaluador'):
             
-            col1, col2, col3 = st.beta_columns([3,2,1])
+            col1, col2 = st.beta_columns([1,1])
 
             with col1:
-                st.text('¿Utilizó Bancos como fuente \n de financiamiento durante el año?')
                 
-                B015a = st.checkbox('Si utilicé financiamiento', help=('Línea de crédito, Crédito de Consumo, Leasing, Factoring, etc.'))
-                B015b = st.checkbox('No utilicé financiamiento')
-                if B015a == True:
+                B015a = st.selectbox('¿Utilizó Bancos como fuente de financiamiento durante el año?',['SI','NO'], help=('Línea de crédito, Crédito de Consumo, Leasing, Factoring, etc.'))
+
+                if B015a == 'SI':
                     varB015 = 1
-                elif B015b == True:
+                else:
                     varB015 = 0
                 
                 varC064 = st.number_input('Obligaciones con Instituciones financieras Corto Plazo en miles de pesos')
                 varC067 = st.number_input('Obligaciones con Instituciones financieras Largo Plazo en miles de pesos')
+                varlogC067 = np.log(varC067+1)
+                
                                 
-                st.text('¿Cuenta con Factoring \ncomo producto\nfinanciero')
-                B011a = st.checkbox('No Cuento con Factoring')        
-                B011b = st.checkbox('Cuento con Factoring')
-                if B011a == True:
+                B011a = st.selectbox('¿Cuenta con Factoring como producto financiero?', ['SI','NO'])        
+               
+                if B011a == 'SI':
                     varB011 = 1
-                elif B011b == True:
+                else:
                     varB011 = 0        
-
-                st.text('¿Utilizó fuentes de financiamiento \ndistintas de bancos como fuente\nde financiamiento durante el año')
-                B018a = st.checkbox('No cuento con fuentes de financiamiento distintas a Bancos')        
-                B018b = st.checkbox('Si cuento con fuentes de financiamiento distintas a Bancos')
-                if B018a == True:
+                B018a = st.selectbox('¿Utilizó fuentes de financiamiento distintas de bancos como fuente de financiamiento durante el año?',['SI','NO'])        
+                
+                if B018a == 'SI':
                     varB018 = 1
-                elif B018b == True:
+                else:
                     varB018 = 0               
 
                 varC056 = st.number_input('Gastos Anuales en miles de pesos')
                 varlogC056 = np.log(varC056+1)
                 varC041 = st.number_input('Total Ingresos Anuales en miles de pesos')
 
-                st.text('¿Cree que la entidad requerirá \ncréditos en los próximos 12\nmeses para refinanciamiento')
-                B099a = st.checkbox('No requerirá crédito para refinanciamiento')        
-                B099b = st.checkbox('Si requerirá crédito para refinanciamiento')
-                if B099a == True:
+                B099a = st.selectbox('¿Cree que la entidad requerirá créditos en los próximos 12 meses para refinanciamiento?', ['SI','NO'])        
+                
+                if B099a == 'Si':
                     varB099 = 1
-                elif B099b == True:
+                else:
                     varB099 = 0 
 
-                st.text('¿La empresa invirtió en adquisición\nreparación y mantención de\nActivo Fijo Vehículos y transporte\n durante el año')
-                B090a = st.checkbox('No invirtió en adquisición, mantención o reparación de AF')        
-                B090b = st.checkbox('Si invirtió en adquisición, mantención o reparación de AF')
-                if B090a == True:
+                B090a = st.selectbox('¿La empresa invirtió en adquisición reparación y mantención de Activo Fijo Vehículos y transporte durante el año?', ['SI', 'NO'])        
+               
+                if B090a == 'SI':
                     varB090 = 1
-                elif B090b == True:
+                else:
                     varB090 = 0 
                 
-                st.text('¿Cree que requerirá créditos\nen los próxmos 12 meses para\nmaquinarias y/o equipos y/o \nherramientas y/o muebles')
-                B098a = st.checkbox('Si requerirá créditos')        
-                B098b = st.checkbox('No requerirá créditos')
-                if B098a == True:
+                B098a = st.selectbox('¿Cree que requerirá créditos en los próxmos 12 meses para maquinarias y/o equipos y/o herramientas y/o muebles?', ['SI', 'NO'])        
+                
+                if B098a == 'SI':
                     varB098 = 1
-                elif B098b == True:
+                else:
                     varB098 = 0 
-
-                st.text('¿La empresa invirtió en adquisición,\nreparación y mantención de\nAF maquinarias herramientas y/o muebles\n durante el año?')
-                B091a = st.checkbox('Si invirtió en adquisición o reparación')        
-                B091b = st.checkbox('No invirtió en adquisición o reparación')
-                if B091a == True:
-                    varB091 = 1
-                elif B091b == True:
-                    varB091 = 0 
-
-
+                
+                st.write('\n')
+                submit_button = st.form_submit_button(label='Evaluar')
 
 
             with col2:            
-                st.text('¿Cree que requerirá \ncrédito en los próximos\n12 meses para Capital de \ntrabajo?')
-                B096a = st.checkbox('Si requeriré crédito para capital de trabajo', help=('Dinero en efectivo en caja o banco, compra materias primas, pago salarios, entre otros'))        
-                B096b = st.checkbox('No requeriré crédito para capital de trabajo')
-                if B096a == True:
-                    varB096 = 1
-                elif B096b == True:
-                    varB096 = 0
-                
-                st.text('¿Su entidad cuenta \ncon Leasing Operacional\ny/o financiero, como \nproductos financieros?')
-                B012a = st.checkbox('No cuento con estos productos')        
-                B012b = st.checkbox('Cuento con estos productos')
-                if B012a == True:
-                    varB012 = 1
-                elif B012b == True:
-                    varB012 = 0
 
-                st.text('¿Cree que NO requerirá \ncrédito en los próximos\n12 meses')
-                B101a = st.checkbox('No requeriré crédito')        
-                B101b = st.checkbox('Si requeriré crédito')
-                if B101a == True:
+                B096a = st.selectbox('¿Cree que requerirá crédito en los próximos 12 meses para Capital de trabajo?', ['SI','NO'], help=('Dinero en efectivo en caja o banco, compra materias primas, pago salarios, entre otros'))        
+                if B096a == 'SI':
+                    varB096 = 1
+                else:
+                    varB096 = 0
+
+                B012a = st.selectbox('¿Su entidad cuenta con Leasing Operacional y/o financiero, como productos financieros?', ['SI','NO'])  
+          
+                if B012a == 'SI':
+                    varB012 = 1
+                else:
+                    varB012 = 0
+        
+                B101a = st.selectbox('¿Cree que NO requerirá crédito en los próximos 12 meses?', ['SI','NO']) 
+                if B101a == 'SI':
                     varB101 = 1
-                elif B101b == True:
+                else:
                     varB101 = 0
 
-                st.text('¿Utilizó Recursos propios\no aumentos de capital\ncomo financiamiento?')
-                B021a = st.checkbox('Si utilicé recursos propios')        
-                B021b = st.checkbox('No utilicé recursos propios')
-                if B021a == True:
+                B021a = st.selectbox('¿Utilizó Recursos propios o aumentos de capital como financiamiento?',['SI', 'NO']) 
+                if B021a == 'SI':
                     varB021 = 1
-                elif B021b == True:
+                else:
                     varB021 = 0
 
-                st.text('¿No realizó ninguna \ninversión o adquisición\nde activos durante el año?')
-                B095a = st.checkbox('No realicé inversión o adquisición de activos')        
-                B095b = st.checkbox('Si realicé inversión o adquisición de activos')
-                if B095a == True:
+                B095a = st.selectbox('¿No realizó ninguna inversión o adquisición de activos durante el año?', ['SI','NO'])       
+                
+                if B095a == 'SI':
                     varB095 = 1
-                elif B095b == True:
+                else:
                     varB095 = 0
 
+                B001a = st.selectbox('¿La empresa cuenta con línea de crédito como producto financiero?', ['SI', 'NO'])   
 
-                st.text('¿La empresa cuenta \ncon línea de crédito\ncomo producto financiero?')
-                B001a = st.checkbox('Si cuento con línea de crédito')        
-                B001b = st.checkbox('No cuento con línea de crédito')
-                if B001a == True:
+                if B001a == 'SI':
                     varB001 = 1
-                elif B001b == True:
+                else:
                     varB001 = 0
-
-
-                st.text('¿La empresa no utilizó \nningún tipo de \nfinanciamientodurante el\naño?')
-                B029a = st.checkbox('No utilizó financiamiento de ningún tipo')        
-                B029b = st.checkbox('Si utilizó financiamiento')
-                if B029a == True:
+             
+                B029a = st.selectbox('¿La empresa no utilizó ningún tipo de financiamientodurante el año?', ['SI','NO'])      
+                if B029a == 'SI':
                     varB029 = 1
-                elif B029b == True:
+                else:
                     varB029 = 0 
-
-                st.text('¿La empresa cuenta con\ntarjeta de crédito\nbancaria, como\nproductos financieros?')
-                B002a = st.checkbox('Si cuenta con tarjeta de crédito')        
-                B002b = st.checkbox('No cuenta con tarjeta de crédito')
-                if B002a == True:
-                    varB002 = 1
-                elif B002b == True:
-                    varB002 = 0
                 
-                st.text('¿La empresa utilizó\nProveedores como fuente\nde financiamiento\ndurante el año?')
-                B017a = st.checkbox('Si utilizó Proveedores como fuente de financiamiento')        
-                B017b = st.checkbox('No utilizó Proveedores como fuente de financiamiento')
-                if B017a == True:
+                B002a = st.selectbox('¿La empresa cuenta con tarjeta de crédito bancaria, como productos financieros?', ['SI', 'NO'])     
+              
+                if B002a == 'SI':
+                    varB002 = 1
+                else:
+                    varB002 = 0
+    
+                B017a = st.selectbox('¿La empresa utilizó Proveedores como fuente de financiamiento durante el año?', ['SI','NO']) 
+                
+                if B017a == 'SI':
                     varB017 = 1
-                elif B017b == True:
+                else:
                     varB017 = 0
+                B091a = st.selectbox('¿La empresa invirtió en adquisición,reparación y mantención de AF maquinarias herramientas y/o muebles durante el año?', ['SI', 'NO'])        
 
+                if B091a == 'SI':
+                    varB091 = 1
+                else:
+                    varB091 = 0 
 
-            with col3:
-                submit_button = st.form_submit_button(label='Evaluar')
+            
+                
 
         if submit_button:
-            df_temp = pd.DataFrame({'B001':varB001,'B002':varB002,'B011':varB011,'B012':varB012,'B018':varB018,'B021':varB021,'B029':varB029,
-                                    'B090':varB090,'B095':varB095,'B096':varB096,'B098':varB098,'B099':varB099,'B101':varB101,'log_C056':varlogC056},
+            df_temp = pd.DataFrame({'B001':varB001,'B002':varB002,'B011':varB011,'B012':varB012,'B021':varB021,'B029':varB029,
+                                    'B090':varB090,'B095':varB095,'B096':varB096,'B099':varB099,'B101':varB101,'log_C056':varlogC056, 'log_C067':varlogC067},
                                      index=(list(range(0,1))))
             #st.dataframe(df_temp)           
             
@@ -199,7 +178,7 @@ def main():
 
 
         with st.form(key='Formulario'):
-            st.text('Ingrese sus datos y un ejecutivo de nuestros servicios financieros asociados \nse contactará a la brevedad con usted')
+            st.text('Ingrese sus datos y un ejecutivo de nuestros servicios financieros asociados se contactará a la brevedad con usted')
             column1, column2, column3 = st.beta_columns([3,2,1])
 
             with column1:
@@ -218,15 +197,15 @@ def main():
 
     if choice == 'Test formulario':
 
-        df_temp2 = pd.DataFrame({'B001':1,'B002':0,'B011':1,'B012':0,'B018':1,'B021':0,'B029':1,
-                                    'B090':0,'B095':1,'B096':0,'B098':1,'B099':0,'B101':1,'log_C056':0},
+        df_temp2 = pd.DataFrame({'B001':1,'B002':0,'B011':1,'B012':0,'B021':1,'B029':0,'B090':1,
+                                    'B095':1,'B096':0,'B099':0,'B101':1,'log_C056':0,'log_C067':0}, 
                                      index=(list(range(0,1))))
 
         st.write(df_temp2)
 
 
         with st.form(key='columns_in_form'):
-            cols = st.beta_columns(14)
+            cols = st.beta_columns(len(df_temp2.columns))
             columns = list(df_temp2.columns)
             for i, col in enumerate(cols):
                 col.selectbox(columns[i],df_temp2[columns[i]], key=str(i))
